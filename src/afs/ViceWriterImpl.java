3f91c7859a8673be5ef58463e5e6482e2fec4ee2
inigoar <inigoarpalacios@yahoo.es>
2019-06-18 12:13:12 +0000

    Update ViceWriterImpl.java

--- src/afs/ViceWriterImpl.java
+++ src/afs/ViceWriterImpl.java
@@ -3,18 +3,40 @@
 package afs;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.io.*;
 
 public class ViceWriterImpl extends UnicastRemoteObject implements ViceWriter {
     private static final String AFSDir = "AFSDir/";
+    RandomAccessFile rnd;
+    ViceImpl lock;
+    ReentrantReadWriteLock cerr;
+    String file;
+    VenusCB callb;
 
-    public ViceWriterImpl(String fileName /* añada los parámetros que requiera */)
-		    throws RemoteException {
+    public ViceWriterImpl(String fileName, String mode,ViceImpl vc,ReentrantReadWriteLock cerrojo,VenusCB callback)
+		    throws RemoteException,FileNotFoundException,IOException {
+                rnd = new RandomAccessFile(AFSDir+fileName,mode);
+                lock = vc;
+                file = fileName;
+                cerr = cerrojo;
+                callb = callback;
+                rnd.seek(0);
     }
-    public void write(byte [] b) throws RemoteException {
+    public void write(byte [] b) throws RemoteException,IOException {
+        rnd.write(b);
         return;
     }
-    public void close() throws RemoteException {
+    public void setLength(long len) throws RemoteException, IOException{
+        rnd.setLength(len);
         return;
     }
-}       
+    public void close() throws RemoteException,IOException {
+        lock.recorrer(file,callb);
+        cerr.writeLock().unlock();
+        lock.lock.unbind(file);
+        rnd.close();
+        return;
+    }
+}     
 
