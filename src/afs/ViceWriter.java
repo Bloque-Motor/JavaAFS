8b73428d4c26c68ceeccc9cf99bd0b1126e60b8e
inigoar <inigoarpalacios@yahoo.es>
2019-06-18 12:12:27 +0000

    Update ViceWriter.java

--- src/afs/ViceWriter.java
+++ src/afs/ViceWriter.java
@@ -2,10 +2,11 @@
 // para completar la carga de un fichero
 package afs;
 import java.rmi.*;
+import java.io.*;
 
 public interface ViceWriter extends Remote {
-    public void write(byte [] b) throws RemoteException;
-    public void close() throws RemoteException;
+    public void write(byte [] b) throws RemoteException,IOException;
+    public void close() throws RemoteException,IOException;
+    public void setLength(long len) throws RemoteException,IOException;
     /* añada los métodos remotos que requiera */
-}       
-
+}   
