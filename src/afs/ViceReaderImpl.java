cfd09a2ac4d69904942d82915e8a469f9ad60887
inigoar <inigoarpalacios@yahoo.es>
2019-06-18 12:11:43 +0000

    Update ViceReaderImpl.java

--- src/afs/ViceReaderImpl.java
+++ src/afs/ViceReaderImpl.java
@@ -3,18 +3,41 @@
 package afs;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.io.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ViceReaderImpl extends UnicastRemoteObject implements ViceReader {
     private static final String AFSDir = "AFSDir/";
+    RandomAccessFile rnd;
+    ViceImpl lock;
+    ReentrantReadWriteLock cerr;
+    String file;
 
-    public ViceReaderImpl(String fileName /* añada los parámetros que requiera */)
-		    throws RemoteException {
+    public ViceReaderImpl(String fileName, String mode,ViceImpl vc,ReentrantReadWriteLock cerrojo)
+		    throws RemoteException,FileNotFoundException {
+                rnd = new RandomAccessFile(AFSDir+fileName,mode);
+                lock = vc;
+                cerr = cerrojo;
+                file = fileName;
     }
-    public byte[] read(int tam) throws RemoteException {
-        return null;
+    public byte[] read(int tam) throws RemoteException,IOException {
+        byte [] buf = new byte[tam];
+        int leido = rnd.read(buf);
+        if(leido == -1){
+            cerr.readLock().unlock();
+            return null;
+        }
+        else if(leido < tam){
+            byte [] leidos2 = new byte[leido];
+            for(int i=0;i<leido;i++) leidos2[i]=buf[i];
+            return leidos2;
+        }
+        return buf;
     }
-    public void close() throws RemoteException {
+    public void close() throws RemoteException,IOException {
+        //cerr.readLock().unlock();
+        lock.lock.unbind(file);
+        rnd.close();
         return;
     }
-}       
-
+}   
