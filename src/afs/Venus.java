5686475a674c0575e304d2579772a4cb61d3d06c
inigoar <inigoarpalacios@yahoo.es>
2019-06-18 12:06:05 +0000

    Update Venus.java

--- src/afs/Venus.java
+++ src/afs/Venus.java
@@ -4,8 +4,19 @@ package afs;
 
 import java.rmi.*; 
 
+
 public class Venus {
-    public Venus() {
+    public String host;
+    public String port;
+    public String tam;
+    public Vice rec;
+    public VenusCB callback;
+
+    public Venus() throws Exception{
+        this.host = System.getenv().get("REGISTRY_HOST");
+        this.port = System.getenv().get("REGISTRY_PORT");
+        this.tam = System.getenv().get("BLOCKSIZE");
+        rec = (Vice)Naming.lookup("rmi://"+ this.host + ":" + this.port +"/AFS");
+        callback = new VenusCBImpl();
     }
 }
-
