fb0fddffbdf95d038addc72832155d68fe0e886c
inigoar <inigoarpalacios@yahoo.es>
2019-06-28 18:29:22 +0000

    Update VenusFile.java

--- src/afs/VenusFile.java
+++ src/afs/VenusFile.java
@@ -10,30 +10,32 @@ public class VenusFile {
     public static final String cacheDir = "Cache/";
 
     RandomAccessFile rnd;
-    ViceWriter escritor;
-    ViceReader lector;
+    ViceWriter writer;
+    ViceReader reader;
     String modo;
     Venus ven;
     String file;
-    private boolean escrito;
     private long tam_mod;
+    private boolean escrito;
 
     public VenusFile(Venus venus, String fileName, String mode) throws RemoteException, IOException, FileNotFoundException {
         try{
-            this.ven=venus;
+            this.ven = venus;
             this.modo = mode;
-            this.file=fileName;
+            this.file = fileName;
             this.escrito = false;
+
             if(mode.equals("rw")){
                 if(!buscar(file)){
+                    
                     rnd = new RandomAccessFile(cacheDir+fileName,mode);
-                    lector = venus.rec.download(fileName,mode,ven.callback);
+                    reader = venus.cl.download(fileName,mode,ven.callback);
                     byte [] leidos;
-                    while((leidos = lector.read(Integer.parseInt(venus.size))) != null){
+                    while((leidos = reader.read(Integer.parseInt(venus.size))) != null){
                         rnd.write(leidos);
                     }
                     rnd.seek(0);
-                    lector.close();
+                    reader.close();
                 }
                 else{
                     rnd = new RandomAccessFile(cacheDir+fileName,mode);
@@ -45,15 +47,15 @@ public class VenusFile {
             }
         }
         catch(FileNotFoundException e){
-            lector = venus.rec.download(fileName,mode,ven.callback);
+            reader = venus.cl.download(fileName,mode,ven.callback);
             rnd = new RandomAccessFile(cacheDir+fileName,"rw");
             byte [] leidos;
-            while((leidos = lector.read(Integer.parseInt(venus.size))) != null){
+            while((leidos = reader.read(Integer.parseInt(venus.size))) != null){
                 rnd.write(leidos);
             }
             rnd.close();
             rnd = new RandomAccessFile(cacheDir+fileName, "r");
-            lector.close();
+            reader.close();
         }
     }
     private boolean buscar(String filename){
@@ -91,26 +93,26 @@ public class VenusFile {
                 rnd.seek(0);
                 byte [] leidos = new byte[Integer.parseInt(ven.size)];
                 int leido = 0;
-                escritor = ven.rec.upload(file,modo,ven.callback);
+                writer = ven.cl.upload(file,modo,ven.callback);
                 while((leido = rnd.read(leidos)) != -1){
                     if(leido < Integer.parseInt(ven.size)){
                         byte [] leidos2 = new byte[leido];
                         for(int i=0;i<leido;i++) leidos2[i]=leidos[i];
-                        escritor.write(leidos2);
+                        writer.write(leidos2);
                     }
                     else{
-                        escritor.write(leidos);
+                        writer.write(leidos);
                     }
                 }
                 if(rnd.length() != this.tam_mod){
-                    escritor.setLength(rnd.length());
+                    writer.setLength(rnd.length());
                 }
-                escritor.close();
+                writer.close();
             }
             else if(rnd.length() != this.tam_mod){
-                escritor = ven.rec.upload(file,modo,ven.callback);
-                escritor.setLength(rnd.length());
-                escritor.close();
+                writer = ven.cl.upload(file,modo,ven.callback);
+                writer.setLength(rnd.length());
+                writer.close();
             }
         }
         rnd.close();
