aa67495ad39c52f76d9fd05f80b5ba7f53f1c38c
inigoar <inigoarpalacios@yahoo.es>
2019-06-28 18:39:03 +0000

    Update VenusFile.java

--- src/afs/VenusFile.java
+++ src/afs/VenusFile.java
@@ -9,7 +9,7 @@ public class VenusFile {
 
     public static final String cacheDir = "Cache/";
 
-    RandomAccessFile rnd;
+    RandomAccessFile raf;
     ViceWriter writer;
     ViceReader reader;
     String modo;
@@ -27,74 +27,92 @@ public class VenusFile {
 
             if(mode.equals("rw")){
                 if(!buscar(file)){
-                    
-                    rnd = new RandomAccessFile(cacheDir+fileName,mode);
+
+                    raf = new RandomAccessFile(cacheDir+fileName,mode);
                     reader = venus.cl.download(fileName,mode,ven.callback);
                     byte [] leidos;
                     while((leidos = reader.read(Integer.parseInt(venus.size))) != null){
-                        rnd.write(leidos);
+                        raf.write(leidos);
                     }
-                    rnd.seek(0);
+                    raf.seek(0);
                     reader.close();
                 }
                 else{
-                    rnd = new RandomAccessFile(cacheDir+fileName,mode);
+                    raf = new RandomAccessFile(cacheDir+fileName,mode);
                 }
-                this.tam_mod = rnd.length();
+                this.tam_mod = raf.length();
             }
             else{
-                rnd = new RandomAccessFile(cacheDir+fileName,mode);
+                raf = new RandomAccessFile(cacheDir+fileName,mode);
             }
         }
         catch(FileNotFoundException e){
             reader = venus.cl.download(fileName,mode,ven.callback);
-            rnd = new RandomAccessFile(cacheDir+fileName,"rw");
+            raf = new RandomAccessFile(cacheDir+fileName,"rw");
             byte [] leidos;
             while((leidos = reader.read(Integer.parseInt(venus.size))) != null){
-                rnd.write(leidos);
+                raf.write(leidos);
             }
-            rnd.close();
-            rnd = new RandomAccessFile(cacheDir+fileName, "r");
+            raf.close();
+            raf = new RandomAccessFile(cacheDir+fileName, "r");
             reader.close();
         }
     }
-    private boolean buscar(String filename){
+
+    private boolean search(String filename){
+
         try{
-            File cache = new File("Cache");
-            for(File f: cache.listFiles()){
-                if(f.getName().equals(filename)){
+
+            File cacheFiles = new File("Cache");
+
+            for(File fileN: cacheFiles.listFiles()){
+
+                if(fileN.getName().equals(filename)){
+
                     return true;
                 }
             }
         }catch(Exception e){
+
             e.printStackTrace();
         }
+
         return false;
     }
+
     public int read(byte[] b) throws RemoteException, IOException {
-        return rnd.read(b);
+
+        return raf.read(b);
     }
+
     public void write(byte[] b) throws RemoteException, IOException {
+
         escrito = true;
-        rnd.write(b);
+        raf.write(b);
         return;
     }
+
     public void seek(long p) throws RemoteException, IOException {
-        rnd.seek(p);
+
+        raf.seek(p);
         return;
     }
+
     public void setLength(long l) throws RemoteException, IOException {
-        rnd.setLength(l);
+
+        raf.setLength(l);
         return;
     }
+
     public void close() throws RemoteException, IOException {
+
         if(this.modo.equals("rw")){
             if(this.escrito == true){
-                rnd.seek(0);
+                raf.seek(0);
                 byte [] leidos = new byte[Integer.parseInt(ven.size)];
                 int leido = 0;
                 writer = ven.cl.upload(file,modo,ven.callback);
-                while((leido = rnd.read(leidos)) != -1){
+                while((leido = raf.read(leidos)) != -1){
                     if(leido < Integer.parseInt(ven.size)){
                         byte [] leidos2 = new byte[leido];
                         for(int i=0;i<leido;i++) leidos2[i]=leidos[i];
@@ -104,17 +122,17 @@ public class VenusFile {
                         writer.write(leidos);
                     }
                 }
-                if(rnd.length() != this.tam_mod){
-                    writer.setLength(rnd.length());
+                if(raf.length() != this.tam_mod){
+                    writer.setLength(raf.length());
                 }
                 writer.close();
             }
-            else if(rnd.length() != this.tam_mod){
+            else if(raf.length() != this.tam_mod){
                 writer = ven.cl.upload(file,modo,ven.callback);
-                writer.setLength(rnd.length());
+                writer.setLength(raf.length());
                 writer.close();
             }
         }
-        rnd.close();
+        raf.close();
     }
 }
