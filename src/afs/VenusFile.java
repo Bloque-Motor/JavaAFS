6540c1c378a71d63a15af78aa46fe3ec2c7c5181
inigoar <inigoarpalacios@yahoo.es>
2019-06-28 18:22:25 +0000

    Update VenusFile.java

--- src/afs/VenusFile.java
+++ src/afs/VenusFile.java
@@ -6,7 +6,9 @@ import java.rmi.*;
 import java.io.*; 
 
 public class VenusFile {
+
     public static final String cacheDir = "Cache/";
+
     RandomAccessFile rnd;
     ViceWriter escritor;
     ViceReader lector;
@@ -15,6 +17,7 @@ public class VenusFile {
     String file;
     private boolean escrito;
     private long tam_mod;
+
     public VenusFile(Venus venus, String fileName, String mode) throws RemoteException, IOException, FileNotFoundException {
         try{
             this.ven=venus;
@@ -26,7 +29,7 @@ public class VenusFile {
                     rnd = new RandomAccessFile(cacheDir+fileName,mode);
                     lector = venus.rec.download(fileName,mode,ven.callback);
                     byte [] leidos;
-                    while((leidos = lector.read(Integer.parseInt(venus.tam))) != null){
+                    while((leidos = lector.read(Integer.parseInt(venus.size))) != null){
                         rnd.write(leidos);
                     }
                     rnd.seek(0);
@@ -45,7 +48,7 @@ public class VenusFile {
             lector = venus.rec.download(fileName,mode,ven.callback);
             rnd = new RandomAccessFile(cacheDir+fileName,"rw");
             byte [] leidos;
-            while((leidos = lector.read(Integer.parseInt(venus.tam))) != null){
+            while((leidos = lector.read(Integer.parseInt(venus.size))) != null){
                 rnd.write(leidos);
             }
             rnd.close();
@@ -86,11 +89,11 @@ public class VenusFile {
         if(this.modo.equals("rw")){
             if(this.escrito == true){
                 rnd.seek(0);
-                byte [] leidos = new byte[Integer.parseInt(ven.tam)];
+                byte [] leidos = new byte[Integer.parseInt(ven.size)];
                 int leido = 0;
                 escritor = ven.rec.upload(file,modo,ven.callback);
                 while((leido = rnd.read(leidos)) != -1){
-                    if(leido < Integer.parseInt(ven.tam)){
+                    if(leido < Integer.parseInt(ven.size)){
                         byte [] leidos2 = new byte[leido];
                         for(int i=0;i<leido;i++) leidos2[i]=leidos[i];
                         escritor.write(leidos2);
@@ -113,4 +116,3 @@ public class VenusFile {
         rnd.close();
     }
 }
-
